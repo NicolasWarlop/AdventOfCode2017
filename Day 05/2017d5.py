235236fc9f4c83ad8d2de0d83128c03de261edc8
Nicolas Warlop <nwarlop@taoadmin.ca>
2017-12-05 13:14:57 -0500

    Advent of Code 2017 - Day 05

---
+++ Day 05/2017d5.py
@@ -0,0 +1,49 @@
+""" 2017d4.py - Advent of code 2017 - day 4"""
+def part_1_solver(instruction_list):
+    """Calculates and increments instructions"""
+    current_index = 0
+    counter = 0
+
+    while(True):
+        try:
+            #jump forward and increment step
+            next_step = current_index + instruction_list[current_index]
+            instruction_list[current_index] += 1
+            current_index = next_step
+            counter += 1
+        except IndexError:
+            return counter
+
+def part_2_solver(instruction_list):
+    """Calculates and increments instructions"""
+    current_index = 0
+    counter = 0
+
+    while(True):
+        try:
+            #jump forward and increment step
+            next_step = current_index + instruction_list[current_index]
+            if instruction_list[current_index] >= 3:
+                instruction_list[current_index] -= 1
+            else:
+                instruction_list[current_index] += 1
+            current_index = next_step
+            counter += 1
+        except IndexError:
+            return counter
+
+def main():
+    """main function"""
+    inputfile = open('d5input.txt', 'r')
+    jump_list = []
+   
+    #split the line based off of the tab delimitor
+    for line in inputfile:
+        jump_list.append(int(line.strip()))
+    
+    print("Part 1: " + str(part_1_solver(jump_list.copy())))
+    print("Part 2: " + str(part_2_solver(jump_list.copy())))
+
+#Main execution
+if __name__ == "__main__":
+    main()
